eba608207005ae773721bae0d698934245ad98a9
Amy <amy.mae.lee@gmail.com>
2016-04-30 07:44:26 -0400

    add GA draft

--- code/genetic.py
+++ code/genetic.py
@@ -1,5 +1,5 @@
-# annealing.py
-# v 1.0  27 April 2016 [KL]
+# genetic.py
+# v 1.0  27 April 2016 [AL]
 
 ######################
 #
@@ -11,8 +11,7 @@
 ######################
 
 # runs Genetic Algorithm for MasterMind
-# Reference: Bernier et al. "Solving MasterMind using GAs and simulated
-#     annealing: a case of dynamic constraint optimization"
+# Reference: Berghmann et al. "Efficient solutions for Mastermind using genetic algorithms"
 
 '''
 Example:
@@ -65,30 +64,29 @@ import MMboard as mm
 
 import numpy as np
 import copy
-import matplotlib.pyplot as plt
 
 
 class GAsim():
 
-    '''An instance of a simulated annealing solution to solve Mastermind.
-
-    These methods implement simulated annealing (SA)
-    Reference: Bernier et al. "Solving MasterMind using GAs and simulated
-       annealing: a case of dynamic constraint optimization"
-
-    Bernier suggests SA finds best solution within a given time constraint
-    but solution is suboptimal; without a time constraint, the algorithm
-    reduces to a random search.'''
+    """An instance of a genetic algorithm to solve Mastermind."""
 
     def __init__(self):
-        # store previous guesses, along with response
-        # key is the string sequence, values is the (b, w) tuple as returned
-        # by guess_code;
-        # supports up to N=10 digits (0-indexed) per position
-        self._prev_guesses = {}
+        # store previous guesses, along with response in the form of a tuple
+        # (code, response) where code is the python array and response is the
+        # (b, w) tuple as returned by guess_code;
+        self._prev_guesses = []
 
-        self._best_guess = None  # stores best guess so far
-        self._best_resp = None  # stores best response so far
+        self.game = None  # game board instance
+
+        # experiment with these parameters
+        self.max_pop = 60
+        self.max_gen = 100
+
+        self.crossover_points = 2
+        self.crossover_prob = 0.5
+        self.crossover_mutation_prob = 0.03
+        self.permutation_prob = 0.03
+        self.inversion_prob = 0.02
 
     def _objective_function(self, guess):
         '''Implement Bernier objective function.
@@ -106,246 +104,201 @@ class GAsim():
         be the secret code. Otherwise, it cannot possibly be the new code,
         but may be close to it.'''
 
-        assert self._sa._codeOK(guess)
+        assert self.game._codeOK(guess)
         C = 0
 
-        for pg in self._prev_guesses:
+        for (c, r) in self._prev_guesses:
             # get what response for a code would have been if the
             # guess is actually the right code, for each previous guess
-            prop_answer = [int(digit) for digit in list(pg)]
-            b, w = self._sa.check_guess(guess=guess, answer=prop_answer)
-            assert (b + w) <= self._sa._L
+            b, w = self.game.check_guess(guess=guess, answer=c)
+            assert (b + w) <= self.game._L
 
-            diffw = self._prev_guesses[pg][1] - w
-            diffb = self._prev_guesses[pg][0] - b
+            diffw = r[1] - w
+            diffb = r[0] - b
             C += abs(diffw) + abs(diffw + diffb)
 
         return C
 
-    def _change_guess(self, guess, repeats=5):
-        '''Takes in a guess as a list of digits, and performs a permutation
-        (change positions of a randomly chosen pair of digits) and/or a mutation
-        (change value of randomly chosen digit).
+    def evolve_population(self, pop_size, gen_size):
+        """
+        :param pop_size: maximum size for a given population
+        :param gen_size: maximum number of population generations
+        """
 
-        `repeats` is the step size (# of changes before returning new guess)'''
+        def crossover(code1, code2):
+            """
+            Up to number of crossovers equal to self.crossover_points, each with probability of self.crossover_prob,
+            crosses code1 with code2
+            """
+            result = copy.copy(code1)
 
-        assert self._sa._codeOK(guess)
-        guess_prime = copy.deepcopy(guess)
+            for i in range(self.game._L):
+                if np.random.rand() <= self.crossover_prob:
+                    result[i] = code2[i]
 
-        for i in xrange(max(repeats, 1)):
+            return result
 
-            # permutation
-            # do not permutate if all but one digits are already right
-            if not ((self._best_guess == guess) and
-                        (self._best_resp[0] >= (self._sa._L - 1))):
 
-                idx1, idx2 = np.random.choice(len(guess), size=2, replace=False)
+        def mutate(code):
+            """
+            With a probability of self.crossover_mutation_prob, crossover is followed by a mutation that replaces
+            the color of one randomly chosen position by a random other color
+            """
+            result = copy.copy(code)
 
-                temp = guess_prime[idx1]
-                guess_prime[idx1] = guess_prime[idx2]
-                guess_prime[idx2] = temp
+            pos = np.random.choice(self.game._L)
+            color = np.random.choice(self.game._C)
+            # try again until mutate to a different color
+            while result[pos] == color:
+                pos = np.random.choice(self.game._L)
+                color = np.random.choice(self.game._C)
 
-            # mutation
-            # do not mutate if all digits found but in wrong spots
-            if not((self._best_guess == guess) and
-                       ((self._best_resp[0] + self._best_resp[1]) == self._sa._L)):
+            result[pos] = color
 
-                idx3 = np.random.choice(len(guess))  # position
-                col = np.random.choice(self._sa._C)  # proposed new color
-                while idx3 == col:
-                    col = np.random.choice(self._sa._C)  # try again
+            return result
 
-                guess_prime[idx3] = col
+        def permute(code):
+            """
+            With a probability of self.permutation_prob, colors of two random positions are switched
+            """
+            result = copy.copy(code)
 
-                # if only one digit is off (i.e. w=1 and b is the maximum
-                # otherwise), don't mutate more than one character
-                if ((self._best_guess == guess) and
-                        (self._best_resp[0] >= (self._sa._L - 1))):
+            if np.random.rand() <= self.permutation_prob:
+                pos_a, pos_b = np.random.choice(self.game._L, 2, replace=False)
+
+                result[pos_a], result[pos_b] = result[pos_b], result[pos_a]
+
+            return result
+
+        def invert(code):
+            """
+            with probability of self.inversion_prob, two randomly chosen positions have their sequence of colors
+            between these two positions inverted
+            """
+            result = copy.copy(code)
+
+            if np.random.rand() <= self.inversion_prob:
+                pos_range = np.random.choice(self.game._L, 2, replace=False)
+                pos1, pos2 = np.sort(pos_range)
+                flipped = np.fliplr([result[pos1:pos2]])[0]
+                result[pos1:pos2] = flipped
+
+            return result
+
+        # TODO vectorize so can loop through generations, entire population at a time (see lecture code)
+
+        # initialize population
+        population = np.random.choice(self.game._C, (pop_size, self.game._L), replace=True)
+        population = [list(item) for item in population]
+
+        elite = []
+        h = 1
+        while len(elite) <= pop_size and h <= gen_size:
+
+            children = []
+
+            for i in range(pop_size-1):
+                child = crossover(population[i], population[i+1])
+
+                if np.random.rand() <= self.crossover_mutation_prob:
+                    child = mutate(child)
+
+                child = permute(child)
+
+                child = invert(child)
+
+                children.append(child)
+
+            # append last one
+            children.append(population[pop_size-1])
+
+            # Calculate fitness score for each child. The closer to 0, the better
+            pop_score = []
+            for c in children:
+                pop_score.append((self._objective_function(c), c))
+
+            # sort population based on fitness in ascending order
+            # pop_score = sorted(pop_score, key=lambda x: x[0])
+
+            # Pick ones where score is 0
+            eligibles = [e for (score, e) in pop_score if score == 0]
+            # print eligibles
+
+            # no good ones, move on to next generation to try again
+            if len(eligibles) == 0:
+                h += 1
+                continue
+
+            # remove duplicates
+            for code in eligibles:
+                if code in elite:
+                    elite.remove(code)
+
+                    # replace the removed duplicate elite code with a random one
+                    elite.append(list(np.random.choice(self.game._C, self.game._L, replace=True)))
+
+            # add eligible to elite
+            for eligible in eligibles:
+                # Make sure we don't overflow our elite size (Ei <= popsize)
+                if len(elite) == pop_size:
                     break
 
-        return guess_prime
+                # TODO
+                if not eligible in elite:
+                    elite.append(eligible)
 
-    def _closeness_score(self, resp):
-        # the higher score, the more correct the answer
-        # we cannot really use this as the objective function, since we need
-        # to actually make a guess before getting this response (it is based
-        # on the actual code and only attainable by committing a guess);
-        # but we can use this to identify the final best answer for reporting
-        # if we did not guess the correct answer in the limited number of
-        # tries/time
+            # Prepare the parent population for the next generation based
+            # on the current generation
+            population = []
+            population.extend(eligibles)
 
-        # inspired by the score for the genetic algorithm, we start with the
-        # following:
-        # score: 10 * black + 1 * white
-        # weights correct digit in correct position more heavily
+            # fill the rest of the population with random codes up to pop_size
+            j = len(eligibles)
+            while j < pop_size:
+                population.append(list(np.random.choice(self.game._C, self.game._L, replace=True)))
+                j += 1
 
-        # response is in the form of a (b, w) tuple
-        # however, we modify this score so that if all the correct digits have
-        # been found (but answer is wrong), this is just as good as finding all
-        # but one correct digits as we can reduce the
-        # dimension of the search to just one type (mutation OR permutation)
+            h += 1
 
-        if (resp[0] != self._sa._L) and ((resp[0] + resp[1]) == self._sa._L):
-            return (self._sa._L - 1) * 10
+        return elite
 
-        return resp[0] * 10 + resp[1]
-
-    def sa(self, guess, resp, init_temp, thermostat, ftol, itol, otol, k):
-        '''Perform simulated annealing.
-
-        Inputs:
-        guess: the initial guess
-        resp: the response returned from the MasterMind solver for that guess
-        init_temp: the initial temperature for the SA algorithm
-        thermostat: the rate at which temperature is decreased
-        itol: a proxy for the maximum time SA can run to solve the game
-              in SA iterations (which is larger than number of guesses made)
-        ftol, otol: other stopping criteria, not currently used
-        k: temperature tuning parameter
-
-        Returns:
-        A tuple containing the (best guess before time expired,
-                                the (b, w) response for the best guess,
-                                the number of iterations<=itol performed) '''
-
-        obj_values = []  # stores objective function values from successive runs
-        curr_guess = copy.deepcopy(guess)
-
-        prev_E = 3 * self._sa._L  # arbitrarily large initial "energy"
-        obj_values.append(prev_E)
-        temperature = init_temp
-
-        best_score = self._closeness_score(resp)  # store best objective function seen so far
-        self._best_guess = guess  # store initial guess as corresponding best guess
-        self._best_resp = resp  # store its response as corresponding best response
-
-        it = 1  # acceptances counter (i.e. actual guesses made)
-        atp = 1  # total number of iterations
-
-        while True:
-
-            L = np.floor(temperature).astype(int)  # step size
-            # L = np.floor(np.exp(temperature)).astype(int)  # alternative step size
-            # L = np.floor(np.sqrt(temperature)).astype(int)  # alternative step size
-            # L = np.floor(np.log(temperature)).astype(int)  # alternative step size
-
-            propose_guess = self._change_guess(curr_guess, L)
-
-            # check whether proposed guess has already been guessed
-            pg_str = ''.join(map(str, propose_guess))
-            while pg_str in self._prev_guesses:
-                propose_guess = self._change_guess(curr_guess)  # guess again
-                pg_str = ''.join(map(str, propose_guess))
-
-            new_E = self._objective_function(propose_guess)
-            delta_E = new_E - prev_E
-
-            # if we find something of equal (not just strictly lower) cost,
-            # we try that as well as the corresponding combination could be good
-            if ((delta_E <= 0) or
-                    (np.random.rand() < np.exp(- 1.0 * delta_E / (k * temperature)))):
-                # how choice was made
-                if delta_E <= 0:
-                    if not(self._sa._nooutput):
-                        print "accepted better or equal, temperature:", temperature
-                else:
-                    if not(self._sa._nooutput):
-                        print "accepted worse (random), temperature:", temperature
-
-                # accept proposal from which to make next guess
-                curr_guess = propose_guess
-                obj_values.append(new_E)
-                prev_E = new_E
-                it += 1
-
-                # actually make the guess
-                response = self._sa.guess_code(propose_guess)
-
-                # keep track of best guess
-                if self._closeness_score(response) >= best_score:
-                    best_score = self._closeness_score(response)
-                    self._best_guess = curr_guess
-                    self._best_resp = response
-
-                # If the response is four colored pegs, the game is won,
-                # the algorithm terminates.
-                # If maximum number of tries reached, the algorithm terminates.
-                if (((response is not None) and response[0]==self._sa._L) or
-                        self._sa.gameover):
-                    # print details of best guess when game has ended
-                    if response[0] != self._sa._L:
-                        if not(self._sa._nooutput):
-                            print 'best guess', self._best_guess, self._best_resp, it
-                            print 'actual', self._sa._code
-                    break
-
-                # Otherwise, cache the response
-                self._prev_guesses[pg_str] = response
-
-            atp += 1
-
-            # temperature adjustment and reannealing
-            temperature = thermostat * temperature
-            if ((temperature <= 2) and
-                    (self._closeness_score(response) < best_score)):
-                # we implemented a variation of reannealing here
-                # if we jumped to a new guess that has a score that is
-                # worse than the current best score, then we have to jump
-                # around more to further explore the space (i.e. "reheat")
-                # so we reset to a higher fraction of the initial temperature
-                temperature = init_temp * 0.8
-
-            # termination conditions
-            if atp > itol:
-                if not(self._sa._nooutput):
-                    print 'itol: maximum iterations reached'
-                    print 'best guess', self._best_guess, self._best_resp, it
-                    print 'actual', self._sa._code
-                break
-
-        if not(self._sa._nooutput):
-            print "plotted"
-            plt.figure()
-            plt.plot(obj_values)
-            plt.title("Objective function")
-            plt.show()
-
-        return self._best_guess, self._best_resp, it
-
-    def runGA(self, cl=4, nc=6, code=None, silent=False):
-        '''Driver: Set up board and run SA algorithm. '''
-
+    def runGA(self, cl=4, nc=6, code=None, silent=False, initial=None):
+        """
+        Set up board and run GA algorithm
+        """
         # if code is provided, it must be in the form of a list or 1-D numpy array
-        self._sa = mm.MMboard(codelength=cl, numcolors=nc, suppress_output=silent)
+        self.game = mm.MMboard(codelength=cl, numcolors=nc, suppress_output=silent)
         if code is None:
-            self._sa.set_code()
+            self.game.set_code()
         else:
-            self._sa.set_code(code)
+            self.game.set_code(code)
 
         # initial guess
-        sa_guess = list(np.random.randint(0, nc, cl))
+        if initial:
+            init_guess = initial
+        elif cl == 4 and nc == 6:
+            init_guess = [1, 1, 2, 3]
+        else:
+            init_guess = list(np.random.randint(0, nc, cl))
+
         # play the guess to get a response of colored (b) and white pegs.
-        response = self._sa.guess_code(sa_guess)
+        response = self.game.guess_code(init_guess)
+        self._prev_guesses.append((init_guess, response))
 
-        # If the response is four colored pegs, the game is won, the algorithm
-        # terminates.
-        # If maximum number of tries reached, the algorithm terminates.
-        if ((response is not None) and response[0] == cl) or self._sa.gameover:
-            return (sa_guess, response, 1)
+        while self.game.gameover is False:
+            eligibles = self.evolve_population(self.max_pop, self.max_gen)
 
-        # Otherwise, cache the response
-        self._prev_guesses[''.join(map(str, sa_guess))] = response
+            # TODO fix this
+            if not eligibles:
+                print 'NO ELIGIBLES!!!!'
 
-        # we fiddled around with these parameters
-        init_temp = 6
-        thermostat = 0.98
-        k = 0.2  # try slightly bigger fractions for larger spaces
-        itol = 10000  # this is a proxy for allowable execution time
+            code = eligibles.pop()
+            while code in [c for (c, r) in self._prev_guesses]:
+                code = eligibles.pop()
 
-        # output is in the form of best guess, best response, number of guesses
-        output = self.sa(sa_guess, response, init_temp, thermostat, 0, itol, 0, k)
+            response = self.game.guess_code(code)
+            self._prev_guesses.append((code, response))
 
-        return output[2]  # number of guesses
 
 if __name__ == "__main__":
     s = GAsim()
